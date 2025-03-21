8225ad6b621c9ccaa0fe7e3a8fb748fec4445f91
reza-lashkari <h23rezla@du.se>
2025-03-21 15:29:51 +0100

    betyg på g

--- client/src/components/RatingForm.jsx
+++ client/src/components/RatingForm.jsx
@@ -1,5 +1,53 @@
+import React, { useState } from "react";
+import Rating from "@mui/material/Rating";
+import Box from "@mui/material/Box";
+import StarIcon from "@mui/icons-material/Star";
+
+// Exempel på etiketter för de olika rating-värdena
+const labels = {
+  0.5: "Utan betyg",
+  1: "Betyg 1",
+  1.5: "Betyg 1,5",
+  2: "Betyg 2",
+  2.5: "Betyg 2,5",
+  3: "Betyg 3",
+  3.5: "Betyg 3,5",
+  4: "Betyg 4",
+  4.5: "Betyg 4,5",
+  5: "Betyg 5"
+};
+
+// Hjälpfunktion för att skapa rätt text
+function getLabelText(value) {
+  return `${value} ${value !== 1 ? "stjärnor" : "stjärna"}`;
+}
+
 function RatingForm() {
-    return ( <h3>RatingForm</h3> );
+  const [value, setValue] = useState(2);
+  const [hover, setHover] = useState(-1);
+
+  return (
+    <>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        getLabelText={getLabelText}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      />
+      {value !== null && (
+        <Box sx={{ ml: 2 }}>
+          {labels[hover !== -1 ? hover : value]}
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default RatingForm;
--- client/src/views/ProductDetail.jsx
+++ client/src/views/ProductDetail.jsx
@@ -1,5 +1,39 @@
+import { Button } from "@mui/material";
+import ProductItemLarge from "../components/ProductItemLarge";
+import { useNavigate } from "react-router-dom";
+import RatingForm from "../components/RatingForm";
+
+
 function ProductDetail() {
-    return ( <h2>ProductDetail</h2> );
+    const product = {
+        "id": 1,
+        "title": "Sommar dröm",
+        "description": "Svart te medsmak av hallon och jordgubb 100g",
+        "price": 23,
+        "imageUrl": "https://quickbutik.imgix.net/50291i/products/636a5f64eef14.jpeg",
+        "createdAt": "2025-03-21T08:50:03.000Z",
+        "updatedAt": "2025-03-21T08:58:59.000Z",
+        "ratings": [
+            {
+                "id": 1,
+                "rating": 4,
+                "createdAt": "2025-03-21T12:53:21.000Z",
+                "updatedAt": "2025-03-21T12:53:21.000Z",
+                "productId": 1
+            }
+        ],
+        "averageRating": "4.0"
+    };
+
+    const navigate =useNavigate();
+    return (
+        <div>  
+            <ProductItemLarge product = {product}/>
+            <Button onClick ={() => navigate (-1)} >Tillbaka</Button>
+            <Button onClick ={() => navigate (`/products/${product.id}/edit`)}>Ändra</Button>
+            <RatingForm />
+        </div>
+    );
 }
 
 export default ProductDetail;
