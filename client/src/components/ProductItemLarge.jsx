8ea65d090c434c166cd38c77fb21d915deafa1bd
reza-lashkari <h23rezla@du.se>
2025-03-21 16:44:15 +0100

    cc

--- client/src/components/ProductItemLarge.jsx
+++ client/src/components/ProductItemLarge.jsx
@@ -1,8 +1,13 @@
+import { Typography } from "@mui/material";
+
+
+
 function ProductItemLarge({ product } ) {
     return ( <div>
         <h3>{product.title}</h3>
-        
-
+        <img width="200" src={product.imageUrl} alt={product.title} />
+        <Typography variant="body2">Beskrivning: {product.description}</Typography>
+        <Typography variant="h6">Pris: {product.price} kr</Typography>
     </div> );
 }
 
